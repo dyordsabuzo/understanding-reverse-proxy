e60551bf1a973deb7c95c04271797e02939e4dd0
George Abuzo <dyordsabuzo@gmail.com>
2022-08-24 14:54:44 +1000

    Setup cloudfront policy response headers

--- cdn/main.tf
+++ cdn/main.tf
@@ -16,13 +16,14 @@ resource "aws_cloudfront_distribution" "cf" {
   }
 
   default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = var.origin_endpoint
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    max_ttl                = 0
-    default_ttl            = 0
+    allowed_methods            = ["GET", "HEAD"]
+    cached_methods             = ["GET", "HEAD"]
+    target_origin_id           = var.origin_endpoint
+    viewer_protocol_policy     = "redirect-to-https"
+    min_ttl                    = 0
+    max_ttl                    = 0
+    default_ttl                = 0
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.policy.id
 
     forwarded_values {
       query_string = true
@@ -31,11 +32,6 @@ resource "aws_cloudfront_distribution" "cf" {
       cookies {
         forward = "all"
       }
-    }
-
-    function_association {
-      event_type   = "viewer-response"
-      function_arn = aws_cloudfront_function.function.arn
     }
   }
 
@@ -75,28 +71,38 @@ resource "aws_route53_record" "endpoints" {
   }
 }
 
-resource "aws_lambda_function" "lambda" {
-  description      = "Lambda edge for setting security headers"
-  function_name    = "lambda-set-security-headers"
-  runtime          = "nodejs14.x"
-  handler          = "lambda.handler"
-  memory_size      = 128
-  timeout          = 10
-  filename         = "lambda-edge.zip"
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-  role             = aws_iam_role.role.arn
-  publish          = true
-}
-
-resource "aws_iam_role" "role" {
-  name               = "iam-role-for-lambda-security-headers"
-  description        = "IAM role for lambda security headers"
-  assume_role_policy = data.aws_iam_policy_document.assume_policy.json
-}
-
 resource "aws_cloudfront_function" "function" {
   name    = "security-header-function"
   comment = "Setup security response headers"
   runtime = "cloudfront-js-1.0"
   code    = file("${path.module}/function.js")
+}
+
+resource "aws_cloudfront_response_headers_policy" "policy" {
+  name    = "security-response-headers-policy"
+  comment = "Security Response headers policy"
+
+  security_headers_config {
+    strict_transport_security {
+      access_control_max_age_sec = 63072000
+      include_subdomains         = true
+      preload                    = true
+      override                   = false
+    }
+
+    content_type_options {
+      override = false
+    }
+
+    xss_protection {
+      protection = true
+      mode_block = true
+      override   = false
+    }
+
+    referrer_policy {
+      referrer_policy = "same-origin"
+      override        = false
+    }
+  }
 }
